4305f4301a7faabeb7c8822a8ee5e6d4411170ad
Willian Bampi <willian.bampi@gmail.com>
2020-02-26 20:20:13 -0300

    Insert user with POST method.
    Signed-off-by: Willian Bampi <willian.bampi@gmail.com>

--- workshopmongo/src/main/java/com/curso/workshop/springbootmongodb/services/UserService.java
+++ workshopmongo/src/main/java/com/curso/workshop/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.curso.workshop.springbootmongodb.domain.User;
+import com.curso.workshop.springbootmongodb.dto.UserDTO;
 import com.curso.workshop.springbootmongodb.repository.UserRepository;
 import com.curso.workshop.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 
 }
