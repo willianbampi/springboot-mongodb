81afcfdd2a1bb5076f5599a06bdce6643276038e
Willian Bampi <willian.bampi@gmail.com>
2020-02-25 21:38:12 -0300

    Get user by id and exception implementation to error of not found user by id.
    Signed-off-by: Willian Bampi <willian.bampi@gmail.com>

--- workshopmongo/src/main/java/com/curso/workshop/springbootmongodb/resources/UserResource.java
+++ workshopmongo/src/main/java/com/curso/workshop/springbootmongodb/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +25,15 @@ public class UserResource {
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	//@GetMapping or @RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findbyId(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 
 }
--- workshopmongo/src/main/java/com/curso/workshop/springbootmongodb/services/UserService.java
+++ workshopmongo/src/main/java/com/curso/workshop/springbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.curso.workshop.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.curso.workshop.springbootmongodb.domain.User;
 import com.curso.workshop.springbootmongodb.repository.UserRepository;
+import com.curso.workshop.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
