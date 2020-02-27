362c7d499fcd9b80960a7c691ed6b92ac17ef608
Willian Bampi <willian.bampi@gmail.com>
2020-02-27 20:11:59 -0300

    Create post with user inside on post.
    Signed-off-by: Willian Bampi <willian.bampi@gmail.com>

--- workshopmongo/src/main/java/com/curso/workshop/springbootmongodb/repository/PostRepository.java
+++ workshopmongo/src/main/java/com/curso/workshop/springbootmongodb/repository/PostRepository.java
@@ -3,9 +3,10 @@ package com.curso.workshop.springbootmongodb.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.curso.workshop.springbootmongodb.domain.Post;
 import com.curso.workshop.springbootmongodb.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 	
 }
