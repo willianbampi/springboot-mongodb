22da892fe83a4104cfaf5c44d28b919f7cd7dabb
Willian Bampi <willian.bampi@gmail.com>
2020-02-27 20:50:33 -0300

    Create comments to add at post.
    Signed-off-by: Willian Bampi <willian.bampi@gmail.com>

--- workshopmongo/src/main/java/com/curso/workshop/springbootmongodb/config/Instantiation.java
+++ workshopmongo/src/main/java/com/curso/workshop/springbootmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.curso.workshop.springbootmongodb.domain.Post;
 import com.curso.workshop.springbootmongodb.domain.User;
 import com.curso.workshop.springbootmongodb.dto.AuthorDTO;
+import com.curso.workshop.springbootmongodb.dto.CommentDTO;
 import com.curso.workshop.springbootmongodb.repository.PostRepository;
 import com.curso.workshop.springbootmongodb.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acorder feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO comment2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().addAll(Arrays.asList(comment3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
